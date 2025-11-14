13e877bc9a36c9d48e1578f2f3497854c3119115
steven <1048900140@qq.com>
2025-11-14 11:42:42 +0800

    add the sort function in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include<iostream>
 
-
+void sort(int* arry,int size)
+{
+	int tmp = 0;
+	for(int i =0;i <size-1;i++)
+	{
+		for(int j =0;j<size-1;j++)
+		{
+			if(arry[i] > arry[j])
+			{
+			tmp = arry[j];
+			arry[j] = arry[i];
+			arry[i] = tmp;
+			}
+		}
+	}
+}
 int main()
 {
     int arry[] = {12,5,6,7,5,3,1};
 
     int size = sizeof(arry)/sizeof(arry[0]);
-    sort(arr,size);
+    sort(arry,size);
     return 0;
 }
